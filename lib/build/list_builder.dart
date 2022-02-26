cbd997853fc6ca6300f55dc56260c059e848eed9
emilio <emilio113kariuki@gmail.com>
2022-02-27 00:42:44 +0300

    February 27, 2022 12:42 AM

--- lib/build/list_builder.dart
+++ lib/build/list_builder.dart
@@ -1,27 +1,43 @@
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 class ListBuilder1 extends StatelessWidget {
   final String imageUrl;
-  const ListBuilder1({Key? key, required this.imageUrl}) : super(key: key);
+  final String category;
+  final String location;
+  final String action;
+  const ListBuilder1(
+      {Key? key,
+      required this.imageUrl,
+      required this.category,
+      required this.location,
+      required this.action})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                shape: BoxShape.rectangle,
-                image: DecorationImage(
-                  image: AssetImage(imageUrl),
-                  fit: BoxFit.cover,
-                )
-              )
-            
-             )
-          ]
-      )
-    );
+        child: Column(children: [
+      Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              shape: BoxShape.rectangle,
+              image: DecorationImage(
+                image: AssetImage(imageUrl),
+                fit: BoxFit.cover,
+              ))),
+      SizedBox(height: size.height * 0.05),
+      Text(category,
+          style: GoogleFonts.roboto(
+              fontSize: 18, color: Color.fromARGB(255, 61, 122, 63))),
+      SizedBox(height: size.height * 0.05),
+      Text(location,
+          style: GoogleFonts.roboto(
+              fontSize: 18, color: Color.fromARGB(255, 68, 70, 68))),
+      SizedBox(height: size.height * 0.05),
+      // ignore: deprecated_member_use
+      FlatButton(onPressed: () {}, child: Text(action),color: Colors.green)
+    ]));
   }
 }

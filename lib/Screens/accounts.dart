e84b6f452b8754a3968d0f4785fb20e22dab875c
emilio <emilio113kariuki@gmail.com>
2022-02-26 19:03:17 +0300

    February 26, 2022 7:03 PM

--- lib/Screens/accounts.dart
+++ lib/Screens/accounts.dart
@@ -20,36 +20,39 @@ class _AccountsState extends State<Accounts> {
       body: SafeArea(
         child: Column(
           children: [
-             Stack(
-               alignment: Alignment.center,
-               clipBehavior: Clip.none,
-               children: [
-                 Container(
-                    width: size.width,
-                    height: size.height * 0.16,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30))
-                    ),
-                    child:Image.asset("assets/images/plant.jpg",fit: BoxFit.cover),
-                  ),
-                  Positioned(
-                    top: size.height * 0.1,
-                    child: Container(
+             Container(
+               child: Stack(
+                 alignment: Alignment.center,
+                 clipBehavior: Clip.none,
+                 children: [
+                   Container(
+                      width: size.width,
+                      height: size.height * 0.16,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.white,width: 5)
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30))
                       ),
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage("assets/images/profile.jpg"),
-                        // backgroundColor: Colors.red,
-                        radius: size.width * 0.15,
-                      ),
-                    ))
-               ],
-               ),
+                      child:Image.asset("assets/images/plant.jpg",fit: BoxFit.cover),
+                    ),
+                    Positioned(
+                      top: size.height * 0.1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white,width: 5)
+                        ),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/images/profile.jpg"),
+                          // backgroundColor: Colors.red,
+                          radius: size.width * 0.15,
+                        ),
+                      ))
+                 ],
+                 ),
+             ),
                SizedBox(
                  height: size.height * 0.1,
                ),
+               AppIcon(iconUrl: 'assets/images/mail.png', func: (){})
 
         ],)
       )

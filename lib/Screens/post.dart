3a50a4ea28aea8f4c58dd21ad05b94c570b20e65
emilio <emilio113kariuki@gmail.com>
2022-02-28 01:15:53 +0300

    February 28, 2022 1:15 AM

--- lib/Screens/post.dart
+++ lib/Screens/post.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:ville/build/build_icon.dart';
 
 class Post extends StatefulWidget {
   Post({Key? key}) : super(key: key);
@@ -13,8 +13,18 @@ class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color.fromARGB(255, 245, 96, 10),
-      body: Text("Post",style: GoogleFonts.roboto(fontSize: 50, color: Colors.white)),
-    );
+        // backgroundColor: Color.fromARGB(255, 245, 96, 10),
+        body: SafeArea(
+          child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AppIcon(
+            iconUrl: "assets/images/back.png",
+            func: () {
+                Navigator.of(context).pop();
+            }),
+              )
+            ]),
+        ));
   }
 }

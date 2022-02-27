4ea6e868dbaef230dd31d81d55d92009d8a46b35
emilio <emilio113kariuki@gmail.com>
2022-02-27 15:57:16 +0300

    February 27, 2022 3:57 PM

---
+++ lib/build/build_circle.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BuildCirlce extends StatelessWidget {
+  const BuildCirlce({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

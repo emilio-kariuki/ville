856daae2ec4267b76922febc9f5e5a3d2bc99fbb
emilio <emilio113kariuki@gmail.com>
2022-02-27 15:56:20 +0300

    February 27, 2022 3:56 PM

--- lib/Screens/accounts.dart
+++ lib/Screens/accounts.dart
@@ -195,6 +195,19 @@ class _AccountsState extends State<Accounts> {
                         itemBuilder: (BuildContext context, int index) => Center(child: ListBuilder1(imageUrl: 'assets/images/plant.jpg',category: "plastic",location: "Dedan Kimathi University",action: "Collect",)),
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10,left: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("Achievements",
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 23,
+                                  color: kGreen,
+                                  fontWeight: FontWeight.w600)),
+                        ],
+                      ),
+                    ),
 
                   ],
                 ),

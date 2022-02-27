a2c2113551f6f4154a2a6b6301a9e920956d7675
emilio <emilio113kariuki@gmail.com>
2022-02-28 01:43:24 +0300

    February 28, 2022 1:43 AM

--- lib/Screens/post.dart
+++ lib/Screens/post.dart
@@ -19,90 +19,92 @@ class _PostState extends State<Post> {
     return Scaffold(
         // backgroundColor: Color.fromARGB(255, 245, 96, 10),
         body: SafeArea(
-      child: Column(children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10, left: 15, top: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("Title",
-                  style: GoogleFonts.robotoCondensed(
-                      fontSize: 23,
-                      color: kGreen,
-                      fontWeight: FontWeight.w600)),
-            ],
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 15, top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Title",
+                    style: GoogleFonts.robotoCondensed(
+                        fontSize: 23,
+                        color: kGreen,
+                        fontWeight: FontWeight.w600)),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: TextFormField(
-            maxLength: 15,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                // icon: Icon(Icons.person,size:30,color: Colors.black),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                focusColor: Colors.red,
-                hintText: "Summary of the waste",
-                // suffixText: "10",
-                // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
-                fillColor: Colors.grey[200]),
-            controller: title,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: TextFormField(
+              maxLength: 15,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  // icon: Icon(Icons.person,size:30,color: Colors.black),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  focusColor: Colors.red,
+                  hintText: "Summary of the waste",
+                  // suffixText: "10",
+                  // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                  fillColor: Colors.grey[200]),
+              controller: title,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10, left: 15, top: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("Description",
-                  style: GoogleFonts.robotoCondensed(
-                      fontSize: 23,
-                      color: kGreen,
-                      fontWeight: FontWeight.w600)),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 15, top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Description",
+                    style: GoogleFonts.robotoCondensed(
+                        fontSize: 23,
+                        color: kGreen,
+                        fontWeight: FontWeight.w600)),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: TextFormField(
-            minLines: 2,
-            maxLines: 10,
-            keyboardType: TextInputType.multiline,
-            // style: TextStyle(height:10),
-            maxLength: 360,
-            decoration: InputDecoration(
-              // contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                // icon: Icon(Icons.person,size:30,color: Colors.black),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                focusColor: Colors.red,
-                hintText: "Summary of the waste",
-                // suffixText: "10",
-                // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
-                fillColor: Colors.grey[200]),
-            controller: description,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: TextFormField(
+              minLines: 2,
+              maxLines: 10,
+              keyboardType: TextInputType.multiline,
+              // style: TextStyle(height:10),
+              maxLength: 360,
+              decoration: InputDecoration(
+                // contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  // icon: Icon(Icons.person,size:30,color: Colors.black),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  focusColor: Colors.red,
+                  hintText: "Summary of the waste",
+                  // suffixText: "10",
+                  // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                  fillColor: Colors.grey[200]),
+              controller: description,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10, left: 15, top: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("Type of Waste",
-                  style: GoogleFonts.robotoCondensed(
-                      fontSize: 23,
-                      color: kGreen,
-                      fontWeight: FontWeight.w600)),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 15, top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Type of Waste",
+                    style: GoogleFonts.robotoCondensed(
+                        fontSize: 23,
+                        color: kGreen,
+                        fontWeight: FontWeight.w600)),
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     ));
   }
 }

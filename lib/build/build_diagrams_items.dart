8924b50f2df14f45fe4353271f003f9ec423948a
emilio <emilio113kariuki@gmail.com>
2022-03-05 14:03:20 +0300

    March 5, 2022 2:03 PM

--- lib/build/build_diagrams_items.dart
+++ lib/build/build_diagrams_items.dart
@@ -1,23 +1,59 @@
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ville/constants/constants.dart';
 
 class DisplayItems extends StatelessWidget {
-  const DisplayItems({Key? key}) : super(key: key);
+  final String imageUrl;
+  final String location;
+  final String category;
+  final int distance;
+  const DisplayItems(
+      {Key? key,
+      required this.imageUrl,
+      required this.category,
+      required this.location,
+      required this.distance})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-      height: size.height * 0.4,
-      width: size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-      child: Column(
-        children: [
-          
-        ],
-      )
-      );
+        height: size.height * 0.4,
+        width: size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+          children: [
+            Container(
+                height: size.height * 0.14,
+                width: size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    shape: BoxShape.rectangle,
+                    image: DecorationImage(
+                      image: AssetImage(imageUrl),
+                      fit: BoxFit.cover,
+                    ))),
+            Text(category,
+                style: GoogleFonts.roboto(
+                    fontSize: 24, color: Color.fromARGB(255, 7, 6, 12))),
+            const SizedBox(height: 5),
+            Text(location,
+                style: GoogleFonts.roboto(fontSize: 24, color: kGreen)),
+            const SizedBox(height: 5),
+            Text(distance.toString(),
+                style: GoogleFonts.roboto(fontSize: 15, color: Colors.grey)),
+            ],
+          ),
+          // Positioned(child: child)
+          ],
+          )
+        );
   }
 }

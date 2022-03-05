43ab829788dad0b64e912efca31bc82bdebd92a1
emilio <emilio113kariuki@gmail.com>
2022-03-05 11:47:49 +0300

    March 5, 2022 11:47 AM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -12,8 +12,6 @@ class _ItemsState extends State<Items> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 245, 96, 10),
-      body: Center(child: Text("Items",style: GoogleFonts.roboto(fontSize: 50, color: Colors.white))),
     );
   }
 }

a804687b2a43d6813edf5ef967c555180721072c
emilio <emilio113kariuki@gmail.com>
2022-03-05 14:12:38 +0300

    March 5, 2022 2:12 PM

--- lib/build/build_diagrams_items.dart
+++ lib/build/build_diagrams_items.dart
@@ -20,52 +20,58 @@ class DisplayItems extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-        height: size.height * 0.4,
+        height: size.height * 0.3,
         width: size.width,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: Colors.white,
         ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-          children: [
-            Container(
-                height: size.height * 0.14,
-                width: size.width,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
-                    shape: BoxShape.rectangle,
-                    image: DecorationImage(
-                      image: AssetImage(imageUrl),
-                      fit: BoxFit.cover,
-                    ))),
-            Text(category,
-                style: GoogleFonts.roboto(
-                    fontSize: 24, color: Color.fromARGB(255, 7, 6, 12))),
-            const SizedBox(height: 5),
-            Text(location,
-                style: GoogleFonts.roboto(fontSize: 24, color: kGreen)),
-            const SizedBox(height: 5),
-            Text('$distance km',
-                style: GoogleFonts.roboto(fontSize: 15, color: Colors.grey)),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
           ),
-          Positioned(
-            top: 5,
-            right: 5,
-            child: AppIcon(
-              iconUrl: "assets/images/mail.png",
-              func: (){},)),
-          Positioned(
-            bottom: 5,
-            right: 5,
-            child: AppIcon(
-              iconUrl: "assets/images/mail.png",
-              func: (){},))
-          ],
-          )
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                  height: size.height * 0.17,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                        image: AssetImage(imageUrl),
+                        fit: BoxFit.cover,
+                      ))),
+              Text(category,
+                  style: GoogleFonts.roboto(
+                      fontSize: 24, color: Color.fromARGB(255, 7, 6, 12))),
+              const SizedBox(height: 5),
+              Text(location,
+                  style: GoogleFonts.roboto(fontSize: 24, color: kGreen)),
+              const SizedBox(height: 5),
+              Text('$distance km',
+                  style: GoogleFonts.roboto(fontSize: 15, color: Colors.grey)),
+              ],
+            ),
+            Positioned(
+              top: 5,
+              right: 5,
+              child: AppIcon(
+                iconUrl: "assets/images/mail.png",
+                func: (){},)),
+            Positioned(
+              bottom: 5,
+              right: 5,
+              child: AppIcon(
+                iconUrl: "assets/images/mail.png",
+                func: (){},))
+            ],
+            ),
+        )
         );
   }
 }

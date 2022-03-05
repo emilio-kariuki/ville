72cef2a6267b0be279e8e1c9062ef5ff0dc152d3
emilio <emilio113kariuki@gmail.com>
2022-03-05 15:22:59 +0300

    March 5, 2022 3:22 PM

---
+++ lib/collect/collect.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Collect extends StatefulWidget {
+  Collect({Key? key}) : super(key: key);
+
+  @override
+  State<Collect> createState() => _CollectState();
+}
+
+class _CollectState extends State<Collect> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

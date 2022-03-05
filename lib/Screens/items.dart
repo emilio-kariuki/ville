5023e6966e7638f17d34a5c0304da3875623d519
emilio <emilio113kariuki@gmail.com>
2022-03-05 12:29:18 +0300

    March 5, 2022 12:29 PM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
@@ -14,50 +16,54 @@ class _ItemsState extends State<Items> {
   @override
   Widget build(BuildContext context) {
     final title = TextEditingController();
-    return Scaffold(
-        body: SafeArea(
-          child: ColorfulSafeArea(
-              overflowRules: const OverflowRules.all(true),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Flexible(
-                          flex:1,
-                          child: TextFormField(
-                            // maxLength: 15,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.search),
-                                border: OutlineInputBorder(
-                                  borderSide: const BorderSide(color: Colors.white, width: 2.0),
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                                // icon: Icon(Icons.person,size:30,color: Colors.black),
-                                filled: true,
-                                hintStyle: TextStyle(color: Color.fromARGB(255, 117, 117, 117),fontWeight: FontWeight.w500),
-                                focusColor: Colors.red,
-                                hintText: "Search for plastics,wastes...",
-                                // suffixText: "10",
-                                // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
-                                fillColor: Color.fromARGB(255, 255, 255, 255)),
-                            controller: title,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          body: SafeArea(
+            child: ColorfulSafeArea(
+                overflowRules: const OverflowRules.all(true),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            flex:1,
+                            child: TextFormField(
+                              // maxLength: 15,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.search),
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(color: Colors.white, width: 2.0),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  // icon: Icon(Icons.person,size:30,color: Colors.black),
+                                  filled: true,
+                                  hintStyle: TextStyle(color: Color.fromARGB(255, 117, 117, 117),fontWeight: FontWeight.w500),
+                                  focusColor: Colors.red,
+                                  hintText: "Search for plastics,wastes...",
+                                  // suffixText: "10",
+                                  // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                                  fillColor: Color.fromARGB(255, 255, 255, 255)),
+                              controller: title,
+                            ),
                           ),
-                        ),
-                        Icon(Icons.refresh,size: 35)
-                      ],
+                          Icon(Icons.refresh,size: 35)
+                        ],
+                      ),
                     ),
-                  ),
-                  TabBar(
-                      tabs: [
-                        Tab(icon: Icon(Icons.flight)),
-                        Tab(icon: Icon(Icons.directions_transit)),
-                        Tab(icon: Icon(Icons.directions_car)),
-                      ],
-                    ),
-                ],
-              )),
-        ));
+                    TabBar(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        tabs: [
+                          Tab(icon: Icon(Icons.flight,color: Colors.black)),
+                          Tab(icon: Icon(Icons.directions_transit,color: Colors.black)),
+                          Tab(icon: Icon(Icons.directions_car,color: Colors.black)),
+                        ],
+                      ),
+                  ],
+                )),
+          )),
+    );
   }
 }

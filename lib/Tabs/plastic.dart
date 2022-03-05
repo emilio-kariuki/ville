30cce39aa8b3458f67c581fbcddbc22ee5ff2c6f
emilio <emilio113kariuki@gmail.com>
2022-03-05 14:41:00 +0300

    March 5, 2022 2:41 PM

--- lib/Tabs/plastic.dart
+++ lib/Tabs/plastic.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
 import 'package:ville/build/build_diagrams_items.dart';
 
@@ -15,59 +16,62 @@ class _PlasticState extends State<Plastic> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/pastic.jpg',
-              location: 'Dedan Kimathi University,Nyeri',
-              distance: 2000,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p2.jpg',
-              location: 'Chuka University,Chuka',
-              distance: 500,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p3.jpg',
-              location: 'Kisii University,Kisii',
-              distance: 240,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p4.jpg',
-              location: 'Mt Kenya University,thika',
-              distance: 60,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p5.jpg',
-              location: 'University of Nairobi,Nairobi',
-              distance: 2000,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p3.jpg',
-              location: 'Multi Media University,Juja',
-              distance: 120,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p2.jpg',
-              location: 'Kiriri University,Nakuru',
-              distance: 670,
-            ),
-            DisplayItems(
-              category: "plastics",
-              imageUrl: 'assets/images/p1.jpg',
-              location: 'Kabarak University,Gilgil',
-              distance: 90,
-            ),
-          ],
+      body: ColorfulSafeArea(
+        overflowRules: OverflowRules.all(true),
+        child: SingleChildScrollView(
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/pastic.jpg',
+                location: 'Dedan Kimathi University,Nyeri',
+                distance: 2000,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p2.jpg',
+                location: 'Chuka University,Chuka',
+                distance: 500,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p3.jpg',
+                location: 'Kisii University,Kisii',
+                distance: 240,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p4.jpg',
+                location: 'Mt Kenya University,thika',
+                distance: 60,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p5.jpg',
+                location: 'University of Nairobi,Nairobi',
+                distance: 2000,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p3.jpg',
+                location: 'Multi Media University,Juja',
+                distance: 120,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p2.jpg',
+                location: 'Kiriri University,Nakuru',
+                distance: 670,
+              ),
+              DisplayItems(
+                category: "plastics",
+                imageUrl: 'assets/images/p1.jpg',
+                location: 'Kabarak University,Gilgil',
+                distance: 90,
+              ),
+            ],
+          ),
         ),
       ),
     );

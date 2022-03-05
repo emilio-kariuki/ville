a980cbfddb03c0278bedec400a020d1558249dfe
emilio <emilio113kariuki@gmail.com>
2022-03-05 12:10:57 +0300

    March 5, 2022 12:10 PM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -15,35 +15,37 @@ class _ItemsState extends State<Items> {
   Widget build(BuildContext context) {
     final title = TextEditingController();
     return Scaffold(
-        body: ColorfulSafeArea(
-            overflowRules: const OverflowRules.all(true),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: TextFormField(
-                        maxLength: 15,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            // icon: Icon(Icons.person,size:30,color: Colors.black),
-                            filled: true,
-                            hintStyle: TextStyle(color: Colors.grey[800]),
-                            focusColor: Colors.red,
-                            hintText: "Summary of the waste",
-                            // suffixText: "10",
-                            // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
-                            fillColor: Colors.grey[200]),
-                        controller: title,
+        body: SafeArea(
+          child: ColorfulSafeArea(
+              overflowRules: const OverflowRules.all(true),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Flexible(
+                        flex:1,
+                        child: TextFormField(
+                          maxLength: 15,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              // icon: Icon(Icons.person,size:30,color: Colors.black),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey[800]),
+                              focusColor: Colors.red,
+                              hintText: "Summary of the waste",
+                              // suffixText: "10",
+                              // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                              fillColor: Colors.grey[200]),
+                          controller: title,
+                        ),
                       ),
-                    ),
-                    
-                  ],
-                ),
-              ],
-            )));
+                      Icon(Icons.refresh)
+                    ],
+                  ),
+                ],
+              )),
+        ));
   }
 }

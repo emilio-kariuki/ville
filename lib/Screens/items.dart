5b44dbc025562d961974856ed0ada777872818bf
emilio <emilio113kariuki@gmail.com>
2022-03-05 12:58:38 +0300

    March 5, 2022 12:58 PM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -28,7 +28,8 @@ class _ItemsState extends State<Items> {
           // ignore: prefer_const_literals_to_create_immutables
           tabs: [
             Tab(
-              child: Indicate(imageUrl: 'assets/images/plant.jpg', action: "Plastic")
+              child: Indicate(imageUrl: 'assets/images/plant.jpg', action: "Plastic"),
+              text: "plastic",
               ),
             Tab(icon: Icon(Icons.directions_transit,color: Colors.black)),
             Tab(icon: Icon(Icons.directions_car,color: Colors.black)),
--- lib/build/type_indicator.dart
+++ lib/build/type_indicator.dart
@@ -12,21 +12,16 @@ class Indicate extends StatelessWidget {
     return Container(
         height: size.height * 0.2,
         width: size.width * 0.1,
-        child: Column(
-          children: [
-            Container(
-                height: size.height * 0.1,
-                width: 40,
-                // width: size.width * 0.26,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    shape: BoxShape.rectangle,
-                    image: DecorationImage(
-                      image: AssetImage(imageUrl),
-                      fit: BoxFit.cover,
-                    ))),
-            Text(action,style: GoogleFonts.roboto(fontSize: 20,color: Colors.grey,fontWeight: FontWeight.w500))
-          ],
-        ));
+        child: Container(
+            height: size.height * 40,
+            width: 40,
+            // width: size.width * 0.26,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                shape: BoxShape.rectangle,
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
+                  fit: BoxFit.cover,
+                ))));
   }
 }

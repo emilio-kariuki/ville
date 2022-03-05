0f7e98c744c1a26484c1d98b75a8d45a1925f773
emilio <emilio113kariuki@gmail.com>
2022-03-05 11:49:28 +0300

    March 5, 2022 11:49 AM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 
 class Items extends StatefulWidget {
-  Items({Key? key}) : super(key: key);
+  const Items({Key? key}) : super(key: key);
 
   @override
   State<Items> createState() => _ItemsState();

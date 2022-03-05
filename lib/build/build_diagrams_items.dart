ea15da6f3ae07a70987e8544fe2f0f0060d18dbf
emilio <emilio113kariuki@gmail.com>
2022-03-05 13:54:57 +0300

    March 5, 2022 1:54 PM

--- lib/build/build_diagrams_items.dart
+++ lib/build/build_diagrams_items.dart
@@ -1,13 +1,23 @@
 import "package:flutter/material.dart";
 
-
 class DisplayItems extends StatelessWidget {
   const DisplayItems({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
-      
-    );
+      height: size.height * 0.4,
+      width: size.width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          
+        ],
+      )
+      );
   }
 }

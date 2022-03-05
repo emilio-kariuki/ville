6a409532342b4494ecd1c7776ee81967f2e94fe2
emilio <emilio113kariuki@gmail.com>
2022-03-05 12:07:55 +0300

    March 5, 2022 12:07 PM

--- lib/Screens/items.dart
+++ lib/Screens/items.dart
@@ -1,5 +1,7 @@
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class Items extends StatefulWidget {
   const Items({Key? key}) : super(key: key);
@@ -9,9 +11,16 @@ class Items extends StatefulWidget {
 }
 
 class _ItemsState extends State<Items> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ColorfulSafeArea(
+        overflowRules: const OverflowRules.all(true),
+        child:Column(children: [
+        
+        ],)
+      )
     );
   }
 }

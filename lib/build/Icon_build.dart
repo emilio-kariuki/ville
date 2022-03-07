a6f6229fb29a480e6fa7552b044ff592537123c1
emilio <emilio113kariuki@gmail.com>
2022-03-08 01:17:42 +0300

    March 8, 2022 1:17 AM

---
+++ lib/build/Icon_build.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ville/build/build_icon_container.dart';
+
+
+class IconBuild extends StatelessWidget {
+  final String iconUrl;
+  const IconBuild({Key? key, required this.iconUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconContainer(
+      child: Image.asset(iconUrl,color: Colors.white),
+    );
+  }
+}

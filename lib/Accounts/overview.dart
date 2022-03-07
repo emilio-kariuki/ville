8c8e523147c255bba22ea452cee5d5d63a27a03c
emilio <emilio113kariuki@gmail.com>
2022-03-07 21:30:16 +0300

    March 7, 2022 9:30 PM

---
+++ lib/Accounts/overview.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+class Overview extends StatefulWidget {
+  const Overview({Key? key}) : super(key: key);
+
+  @override
+  State<Overview> createState() => _OverviewState();
+}
+
+class _OverviewState extends State<Overview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("EcoVille")
+          ],
+        ),
+      ),
+    );
+  }
+}

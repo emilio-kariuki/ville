1b6ada326723aa2bc448a217ae152ae77eebac07
emilio <emilio113kariuki@gmail.com>
2022-03-13 11:48:42 +0300

    March 13, 2022 11:48 AM

--- lib/Accounts/overview.dart
+++ lib/Accounts/overview.dart
@@ -47,59 +47,60 @@ class _OverviewState extends State<Overview> {
           // Positioned(
           //   child: ,
           // ),
-          )
+          ),
+          SizedBox(height: size.height*0.13),
 
-          // SizedBox(
-          //   height: size.height * 0.06,
-          //   width: size.width * 0.7,
-          //   child: ElevatedButton(
-          //     style: ButtonStyle(
-          //       backgroundColor: MaterialStateProperty.all(
-          //           Color.fromARGB(255, 14, 14, 20)),
-          //       // MaterialStateProperty<Color?>?
-          //       shape: MaterialStateProperty.all<
-          //           RoundedRectangleBorder>(
-          //         RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(30.0),
-          //           side: BorderSide(
-          //             color: Color.fromARGB(255, 14, 14, 20),
-          //             width: 2.0,
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     child: Text('Submit',
-          //         style: GoogleFonts.roboto(fontSize: 20)),
-          //     onPressed: (){
-          //     },
-          //   ),
-          // ),
-          // SizedBox(height:5),
-          // SizedBox(
-          //   height: size.height * 0.06,
-          //   width: size.width * 0.7,
-          //   child: ElevatedButton(
-          //     style: ButtonStyle(
-          //       backgroundColor: MaterialStateProperty.all(
-          //           Color.fromARGB(255, 14, 14, 20)),
-          //       // MaterialStateProperty<Color?>?
-          //       shape: MaterialStateProperty.all<
-          //           RoundedRectangleBorder>(
-          //         RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(30.0),
-          //           side: BorderSide(
-          //             color: Color.fromARGB(255, 14, 14, 20),
-          //             width: 2.0,
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     child: Text('Submit',
-          //         style: GoogleFonts.roboto(fontSize: 20)),
-          //     onPressed: (){
-          //     },
-          //   ),
-          // ),
+          SizedBox(
+            height: size.height * 0.06,
+            width: size.width * 0.7,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(255, 14, 14, 20)),
+                // MaterialStateProperty<Color?>?
+                shape: MaterialStateProperty.all<
+                    RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(
+                      color: Color.fromARGB(255, 14, 14, 20),
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+              child: Text('Login',
+                  style: GoogleFonts.roboto(fontSize: 20)),
+              onPressed: (){
+              },
+            ),
+          ),
+          SizedBox(height:5),
+          SizedBox(
+            height: size.height * 0.06,
+            width: size.width * 0.7,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(255, 14, 14, 20)),
+                // MaterialStateProperty<Color?>?
+                shape: MaterialStateProperty.all<
+                    RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(
+                      color: Color.fromARGB(255, 14, 14, 20),
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+              child: Text('SignUp',
+                  style: GoogleFonts.roboto(fontSize: 20)),
+              onPressed: (){
+              },
+            ),
+          ),
         ],
       ),
     );

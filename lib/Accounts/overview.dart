217c237590160572bd4ffe1e59f0d4e0b7ff00d6
emilio <emilio113kariuki@gmail.com>
2022-03-13 11:07:42 +0300

    March 13, 2022 11:07 AM

--- lib/Accounts/overview.dart
+++ lib/Accounts/overview.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ville/build/Icon_build.dart';
+import 'package:ville/constants/constants.dart';
 
 class Overview extends StatefulWidget {
   const Overview({Key? key}) : super(key: key);
@@ -16,74 +17,82 @@ class _OverviewState extends State<Overview> {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            SizedBox(height:30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // IconBuild(iconUrl: "assets/images/pin.png"),
-                Text("EcoVille",
-                    style: GoogleFonts.roboto(
-                        fontSize: 25,
-                        color: Color.fromARGB(255, 17, 17, 17),
-                        fontWeight: FontWeight.w500)),
+            Container(
+              height:size.height * 0.4,
+              width:size.width,
+              decoration:BoxDecoration(
+                color:kGreen,
+
+              )
+            )
+            
+           
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     // IconBuild(iconUrl: "assets/images/pin.png"),
+            //     Text("EcoVille",
+            //         style: GoogleFonts.roboto(
+            //             fontSize: 25,
+            //             color: Color.fromARGB(255, 17, 17, 17),
+            //             fontWeight: FontWeight.w500)),
                 
-              ],
-            ),
-            SizedBox(height:10),
-            Image.asset('assets/images/over.jpg', width: size.width),
-            SizedBox(height:10),
-            SizedBox(
-              height: size.height * 0.06,
-              width: size.width * 0.7,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(255, 14, 14, 20)),
-                  // MaterialStateProperty<Color?>?
-                  shape: MaterialStateProperty.all<
-                      RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      side: BorderSide(
-                        color: Color.fromARGB(255, 14, 14, 20),
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                ),
-                child: Text('Submit',
-                    style: GoogleFonts.roboto(fontSize: 20)),
-                onPressed: (){
-                },
-              ),
-            ),
-            SizedBox(height:5),
-            SizedBox(
-              height: size.height * 0.06,
-              width: size.width * 0.7,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(255, 14, 14, 20)),
-                  // MaterialStateProperty<Color?>?
-                  shape: MaterialStateProperty.all<
-                      RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      side: BorderSide(
-                        color: Color.fromARGB(255, 14, 14, 20),
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                ),
-                child: Text('Submit',
-                    style: GoogleFonts.roboto(fontSize: 20)),
-                onPressed: (){
-                },
-              ),
-            ),
+            //   ],
+            // ),
+           
+            // SizedBox(
+            //   height: size.height * 0.06,
+            //   width: size.width * 0.7,
+            //   child: ElevatedButton(
+            //     style: ButtonStyle(
+            //       backgroundColor: MaterialStateProperty.all(
+            //           Color.fromARGB(255, 14, 14, 20)),
+            //       // MaterialStateProperty<Color?>?
+            //       shape: MaterialStateProperty.all<
+            //           RoundedRectangleBorder>(
+            //         RoundedRectangleBorder(
+            //           borderRadius: BorderRadius.circular(30.0),
+            //           side: BorderSide(
+            //             color: Color.fromARGB(255, 14, 14, 20),
+            //             width: 2.0,
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //     child: Text('Submit',
+            //         style: GoogleFonts.roboto(fontSize: 20)),
+            //     onPressed: (){
+            //     },
+            //   ),
+            // ),
+            // SizedBox(height:5),
+            // SizedBox(
+            //   height: size.height * 0.06,
+            //   width: size.width * 0.7,
+            //   child: ElevatedButton(
+            //     style: ButtonStyle(
+            //       backgroundColor: MaterialStateProperty.all(
+            //           Color.fromARGB(255, 14, 14, 20)),
+            //       // MaterialStateProperty<Color?>?
+            //       shape: MaterialStateProperty.all<
+            //           RoundedRectangleBorder>(
+            //         RoundedRectangleBorder(
+            //           borderRadius: BorderRadius.circular(30.0),
+            //           side: BorderSide(
+            //             color: Color.fromARGB(255, 14, 14, 20),
+            //             width: 2.0,
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //     child: Text('Submit',
+            //         style: GoogleFonts.roboto(fontSize: 20)),
+            //     onPressed: (){
+            //     },
+            //   ),
+            // ),
           ],
         ),
       ),

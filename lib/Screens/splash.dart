b59d96017c5e058bb036e903233d882574264fff
emilio <emilio113kariuki@gmail.com>
2022-03-14 01:53:46 +0300

    March 14, 2022 1:53 AM

---
+++ lib/Screens/splash.dart
@@ -0,0 +1,37 @@
+
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ville/Screens/introduction_screens.dart';
+
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        const Duration(seconds: 4),
+        () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => Introduction()),
+            ));
+            
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(239, 35, 156, 255),
+      extendBodyBehindAppBar: true,
+      body: Center(child: Image.asset('assets/images/ecoville.png'),)
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:ville/Screens/Home.dart';
-import 'package:ville/Screens/introduction_screens.dart';
 import 'package:flutter/material.dart';
+import 'package:ville/Screens/splash.dart';
 // import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp( );
   runApp(const MaterialApp(
-    home: Introduction(),
+    home: Splash(),
     debugShowCheckedModeBanner: false,
   ));
 }

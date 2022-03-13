3de1620837ff7ca392fa16d728f09d3fe7f1d6c9
emilio <emilio113kariuki@gmail.com>
2022-03-13 11:24:35 +0300

    March 13, 2022 11:24 AM

--- lib/Accounts/overview.dart
+++ lib/Accounts/overview.dart
@@ -104,19 +104,18 @@ class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0, size.height - 90);
+    path.lineTo(0, size.height - 80);
     var controllPoint = Offset(50, size.height);
-    var endPoint = Offset(size.width/2 , size.height);
+    var endPoint = Offset(size.width / 2, size.height);
     path.quadraticBezierTo(
         controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
     return path;
-   
   }
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    throw UnimplementedError();
+    return shouldReclip(oldClipper);
   }
 }

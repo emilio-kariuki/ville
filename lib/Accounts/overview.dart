d57805ef7ebcc0b76b68f90357f8eb939c0f6631
emilio <emilio113kariuki@gmail.com>
2022-03-13 11:18:44 +0300

    March 13, 2022 11:18 AM

--- lib/Accounts/overview.dart
+++ lib/Accounts/overview.dart
@@ -104,9 +104,9 @@ class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0, size.height - 50);
+    path.lineTo(0, size.height - 90);
     var controllPoint = Offset(50, size.height);
-    var endPoint = Offset(size.width / 2, size.height);
+    var endPoint = Offset(size.width/2 , size.height);
     path.quadraticBezierTo(
         controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
     path.lineTo(size.width, size.height);

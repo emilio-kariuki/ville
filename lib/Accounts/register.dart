1bfd0e93ee56f0cf11953a5f6adec68818aebac2
emilio <emilio113kariuki@gmail.com>
2022-03-14 00:41:26 +0300

    March 14, 2022 12:41 AM

--- lib/Accounts/register.dart
+++ lib/Accounts/register.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: unused_import
 
-import 'package:firebase/firebase.dart';
+// import 'package:firebase/firebase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';

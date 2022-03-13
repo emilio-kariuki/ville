e806b8a8514e9d239ca017c8a2da3f23ab8eac9d
emilio <emilio113kariuki@gmail.com>
2022-03-13 23:49:00 +0300

    March 13, 2022 11:49 PM

---
+++ lib/Accounts/auth.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: unused_local_variable, avoid_print, empty_catches, unused_element, nullable_type_in_catch_clause
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthService {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  //Register user
+  Future<User?> register(
+      String email, String password, BuildContext context) async {
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(e.message.toString()),
+          duration: Duration(milliseconds: 600)));
+    } catch (e) {
+      print(e);
+    }
+
+    Future<User?> login(
+        String email, String password, BuildContext context) async {
+      try {
+        UserCredential userCredential = await auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        return userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        print(e);
+      } catch (e) {
+        print(e);
+      }
+    }
+
+    // FirebaseAuth auth = FirebaseAuth.instance;
+
+    Future<void> _signOut() async {
+      await auth.signOut();
+    }
+
+    //login user
+    // Future<User?> login(String email, String password){
+
+    // }
+  }
+}
--- lib/Accounts/register.dart
+++ lib/Accounts/register.dart
@@ -1,11 +1,15 @@
 // ignore_for_file: unused_import
 
+import 'package:firebase/firebase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ville/Accounts/auth.dart';
 import 'package:ville/build/Icon_build.dart';
 import 'package:ville/build/social.dart';
 import 'package:ville/constants/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -15,6 +19,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool loading = false;
   final email = TextEditingController();
   final password = TextEditingController();
   @override
@@ -95,7 +100,71 @@ class _LoginState extends State<Login> {
               controller: password,
             ),
           ),
-          
+          loading
+                    ? CircularProgressIndicator()
+                    : Card(
+                        shadowColor: Colors.green[900],
+                        color: Colors.white,
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: FlatButton(
+                            onPressed: () async {
+                              setState(() {
+                                loading = true;
+                              });
+                              if (email.text == "") {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text("email required"),
+                                  backgroundColor: Colors.red,duration: Duration(milliseconds: 500),
+                                ));
+                              } else if (password.text == "") {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text("password required"),
+                                  backgroundColor: Colors.red,duration: Duration(milliseconds: 500),
+                                ));
+                              } else {
+                                var result = await AuthService().register(
+                                    email.text, password.text, context);
+                                if (result != null) {
+                                  print("success");
+                                  print(result.email);
+                                }
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text("Account created "),
+                                  backgroundColor: Colors.blue[900],duration: Duration(milliseconds: 500),
+                                ));
+                              }
+                              setState(() {
+                                loading = false;
+                              });
+                              // setState(() {
+                              //   Navigator.of(context).pushReplacement(
+                              //       MaterialPageRoute(
+                              //           builder: (context) => Accounts()));
+                              // });
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            // icon: Icon(
+                            //   Icons.create,
+                            //   size: 24,
+                            //   color: Colors.black,
+                            // ),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                              child: Text("Sign Up",
+                                  style: GoogleFonts.robotoCondensed(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                            )),
+                      ),
           Row(children: [
             Expanded(
               // ignore: unnecessary_new

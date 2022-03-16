b2c1704f864c1bea1efb93de7078e8ac88cb3e45
emilio <emilio113kariuki@gmail.com>
2022-03-16 14:12:40 +0300

    March 16, 2022 2:12 PM

---
+++ lib/collect/location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapCollect extends StatefulWidget {
+  MapCollect({Key? key}) : super(key: key);
+
+  @override
+  State<MapCollect> createState() => _MapCollectState();
+}
+
+class _MapCollectState extends State<MapCollect> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

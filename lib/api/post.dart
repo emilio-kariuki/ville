333a59dacdacb2ac7a392aebf6824b8f896d62d1
emilio <emilio113kariuki@gmail.com>
2022-03-21 21:53:20 +0300

    March 21, 2022 9:53 PM

--- lib/api/post.dart
+++ lib/api/post.dart
@@ -1,10 +1,21 @@
+import 'dart:convert';
+
 import "package:flutter/material.dart";
 import "package:http/http.dart" as http;
 
 class Api {
   postData() async {
     var response = await http.post(Uri.parse(
-        "https://greendemo-333401-default-rtdb.firebaseio.com/post.json"));
-        
+        "https://greendemo-333401-default-rtdb.firebaseio.com/post.json"),body: json.encode(
+          {
+          'location': "Dedan Kimathi University",
+          'waste_type': "Plastic",
+          'image': "plastic image",
+          'description': "fresh plastic",
+          'name': "Plastic",
+    
+        }
+        ));
+
   }
 }

62383e4cb19daaba3b1187151b9cfa3e00c316b9
emilio <emilio113kariuki@gmail.com>
2022-03-27 15:44:34 +0300

    March 27, 2022 3:44 PM

--- lib/Stream/data_stream.dart
+++ lib/Stream/data_stream.dart
@@ -1,6 +1,18 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:ville/firebase.dart/post_model.dart';
 
 class DataStream {
   final _database = FirebaseDatabase.instance.ref();
-  
+
+  Stream<List<PostModel>> getPostStream() {
+    final orderStream = _database.child('post').onValue;
+    final results = orderStream.map((event) {
+      final Map<String, dynamic> orderMap = event.snapshot.value as Map<String, dynamic>;
+      final orderList = orderMap.entries.map((element) {
+        return PostModel.fromRTDB(Map<String, dynamic>.from(element.value));
+      }).toList();
+      return orderList;
+    });
+    return results;
+  }
 }

f7f38fe1bf485bd35fdaf088d309d382541d873d
emilio <emilio113kariuki@gmail.com>
2022-03-28 13:31:21 +0300

    March 28, 2022 1:31 PM

--- lib/Tabs/plastic.dart
+++ lib/Tabs/plastic.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:colorful_safe_area/colorful_safe_area.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:ville/Stream/data_stream.dart';
 import 'package:ville/build/build_diagrams_items.dart';
@@ -15,6 +16,12 @@ class Plastic extends StatefulWidget {
 }
 
 class _PlasticState extends State<Plastic> {
+  final _database = FirebaseDatabase.instance.ref();
+  void readData(){
+    _database.once().then((DataSnapshot snapshot) async {
+      print('Data : ${snapshot.value}');
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +36,7 @@ class _PlasticState extends State<Plastic> {
                   builder: (context, snapshot) {
                     final datalist = <DisplayItems>[];
                     if (snapshot.hasData) {
-                      final myOrders = snapshot.data as List<PostModel>;
+                      final myOrders = Map<String, dynamic>.from(snapshot.data!.value);
                       print(myOrders);
                       datalist.addAll(myOrders.map((order) => DisplayItems(
                             func: () {},

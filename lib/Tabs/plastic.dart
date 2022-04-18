76dac0ff8334356e6728bf426e559a4d34f1b537
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-18 21:08:15 +0300

    April 18, 2022 9:08 PM

--- lib/Tabs/plastic.dart
+++ lib/Tabs/plastic.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -28,19 +29,38 @@ class _PlasticState extends State<Plastic> {
       body: ColorfulSafeArea(
         overflowRules: OverflowRules.all(true),
         child: SingleChildScrollView(
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              DisplayItems(
-                func: () {},
-                category: "plastics",
-                imageUrl: 'assets/imagesd/p2.jpg',
-                location: 'Chuka University,Chuka',
-                distance: 500,
-              ),
-            ],
+            child: Expanded(
+          flex: 1,
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                    child: CircularProgressIndicator(color: Colors.white));
+              }
+
+              final data = snapshot.requireData;
+
+              return ListView.builder(
+                itemCount: data.size,
+                itemBuilder: (context, index) {
+                  return DisplayItems(
+                    func: () {},
+                    category: "${data.docs[index]['type']}",
+                    imageUrl: '${data.docs[index]['url']}',
+                    location: '${data.docs[index]['location']}',
+                    distance: 500,
+                  );
+                },
+              );
+            },
           ),
-        ),
+        )),
       ),
     );
   }
